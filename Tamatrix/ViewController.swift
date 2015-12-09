1ac46856b9fd557c75036a389385fb5bb888fca4
Christopher Bonhage <bonhage@apple.com>
2015-12-09 14:42:08 -0800

    Initial Commit

---
+++ Tamatrix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tamatrix
+//
+//  Created by Christopher Bonhage on 12/9/15.
+//  Copyright © 2015 Christopher Bonhage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
