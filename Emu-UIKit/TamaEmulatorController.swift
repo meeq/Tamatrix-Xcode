16ce1fb87d43a673c8584551c7d5d5f5cb47b658
Christopher Bonhage <bonhage@apple.com>
2015-12-31 22:14:20 -0800

    Emulator works!

--- Emu-UIKit/TamaEmulatorController.swift
+++ Emu-UIKit/TamaEmulatorController.swift
@@ -12,6 +12,7 @@ let tamaClock: Int = 16000000
 let tamaFps: Int = 15
 let tamaRunCycles: Int32 = Int32(tamaClock / tamaFps)
 let tamaDramSize: Int = 512
+let nanosPerSecond: Double = 1000000000.0
 
 let tamaScreenWidth = 48
 let tamaScreenHeight = 32
@@ -35,8 +36,7 @@ struct TamaIcons : OptionSetType {
 class TamaEmulatorController: NSObject {
 
     var romData: UnsafeMutablePointer<UnsafeMutablePointer<UInt8>>?
-    var tamaPointer: UnsafeMutablePointer<Tamagotchi>?
-    var tama: Tamagotchi?
+    var tama: UnsafeMutablePointer<Tamagotchi>?
     var dram = [UInt8](count: tamaDramSize, repeatedValue: 0)
     var display = Display()
     var frameCount: UInt = 0
@@ -45,15 +45,14 @@ class TamaEmulatorController: NSObject {
     override init() {
         super.init()
         self.romData = loadRoms(self.romDataPath()!)
-        self.tamaPointer = tamaInit(self.romData!, self.eepromPath()!)
-        self.tama = self.tamaPointer!.memory
+        self.tama = tamaInit(self.romData!, self.eepromPath()!)
         benevolentAiInit()
         udpInit(self.udpServerHost()!)
     }
 
     deinit {
-        if self.tamaPointer != nil {
-            tamaDeinit(self.tamaPointer!)
+        if self.tama != nil {
+            tamaDeinit(self.tama!)
         }
         if self.romData != nil {
             freeRoms(self.romData!)
@@ -92,50 +91,56 @@ class TamaEmulatorController: NSObject {
     }
 
     func renderDisplay() {
+        let tama = self.tama!.memory
         // TODO Fix this pointer type conversion nonsense
         // Convert (char *) Tuple to uint8_t Array
         var i = 0
-        for (_, value) in Mirror(reflecting: self.tama!.dram).children {
-            self.dram[i] = UInt8(value as! Int8)
+        for (_, value) in Mirror(reflecting: tama.dram).children {
+            self.dram[i] = unsafeBitCast(value as! Int8, UInt8.self)
             i += 1
         }
         // Convert uint8_t Array into (uint8_t *)
         self.dram.withUnsafeMutableBufferPointer { (inout ptr: UnsafeMutableBufferPointer<UInt8>) -> () in
-            let lcd = self.tama!.lcd
-            lcdRender(ptr.baseAddress, lcd.sizex, lcd.sizey, &self.display)
+            lcdRender(ptr.baseAddress, tama.lcd.sizex, tama.lcd.sizey, &self.display)
         }
+    }
+
+    func showFrame() {
+        lcdShow(&self.display)
+        udpSendDisplay(&self.display)
+        tamaDumpHw(self.tama!.memory.cpu)
+        benevolentAiDump()
+    }
+
+    func runFrameSync() -> (timespec, timespec) {
+        var frameStartTime = timespec()
+        clock_gettime(CLOCK_MONOTONIC, &frameStartTime)
+        tamaRun(self.tama!, tamaRunCycles - 1)
+        self.renderDisplay()
+        udpTick()
+        if (self.frameCount & 15) == 0 {
+            self.showFrame()
+        }
+        var frameEndTime = timespec()
+        clock_gettime(CLOCK_MONOTONIC, &frameEndTime)
+        self.frameCount += 1
+        return (frameStartTime, frameEndTime)
     }
 
     func runFrameAsync() {
         dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)) {
-            var frameStartTime = timespec()
-            clock_gettime(CLOCK_MONOTONIC, &frameStartTime)
-            tamaRun(self.tamaPointer!, tamaRunCycles - 1)
-            self.renderDisplay()
-            udpTick()
-            if (self.frameCount & 15) == 0 {
-                dispatch_async(dispatch_get_main_queue()) {
-                    lcdShow(&self.display)
-                    udpSendDisplay(&self.display)
-                    tamaDumpHw(self.tama!.cpu)
-                    benevolentAiDump()
-                }
-            }
-            var frameEndTime = timespec()
-            clock_gettime(CLOCK_MONOTONIC, &frameEndTime)
-            self.frameCount += 1
+            let (frameStartTime, frameEndTime) = self.runFrameSync()
             self.dispatchNextFrame(frameStartTime, frameEndTime: frameEndTime)
         }
     }
 
     func dispatchNextFrame(frameStartTime: timespec, frameEndTime: timespec) {
-        var frameDuration: Double // In Microseconds
-        frameDuration = Double(frameEndTime.tv_nsec - frameStartTime.tv_nsec) / 1000.0
-        frameDuration += Double(frameEndTime.tv_sec - frameStartTime.tv_sec) * 1000000.0
-        let frameDelay: Double = (1000000.0 / Double(tamaFps)) - frameDuration
+        var frameDuration = Double(frameEndTime.tv_sec - frameStartTime.tv_sec)
+        frameDuration += Double(frameEndTime.tv_nsec - frameStartTime.tv_nsec) / nanosPerSecond
+        let frameDelay = NSTimeInterval((1.0 / Double(tamaFps)) - frameDuration)
         if frameDelay > 0 {
             let timer = NSTimer(
-                timeInterval: NSTimeInterval(frameDelay / 1000000.0),
+                timeInterval: frameDelay,
                 target: self,
                 selector: "runFrameAsync",
                 userInfo: nil,
